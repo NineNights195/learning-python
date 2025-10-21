bddd7b73dcb9ab0f80853daff20bb4f688d67d8c
Thanarat Ruengkum <ninenights19534@gmail.com>
2025-10-22 04:44:43 +0700

    Learn variable

---
+++ learn-the-basics/var.py
@@ -0,0 +1,15 @@
+# Variable is a container that stores a value
+
+first_name = "Thanarat"     # String = word
+age = 17                    # Integer = number
+price = 20.99               # Float = number with decimal point
+is_student = True           # Boolean = true/false statement (binary)
+
+print(f"Hello {first_name}")        # f = format (use when add variable in string)
+print(f"You are {age} years old")   # To add variable in string, use {}
+print(f"The price is {price} bath")
+print(f"Are you a student?: {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
