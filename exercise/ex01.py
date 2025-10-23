d88f68b524f2869a4138af5640b839fb3ed46c09
NineNights195 <ninenights19534@gmail.com>
2025-10-23 10:48:42 +0700

    Try some exercises after learn

---
+++ exercise/ex01.py
@@ -0,0 +1,6 @@
+# Exercise 1: Rectangle Area Calculator
+
+print("Rectangle Area Calculator")
+w = float(input("Enter width: "))
+l = float(input("Enter length: "))
+print(f"Area = {w*l}")
---
+++ exercise/ex02.py
@@ -0,0 +1,7 @@
+# Exercise 2: Shopping Cart Program
+
+item = input("Enter your item: ")
+price = float(input("Price: "))
+quantity = int(input("Quantity: "))
+print(f"You have bought {quantity}x {item}/s")
+print(f"Your total is {price*quantity}")
