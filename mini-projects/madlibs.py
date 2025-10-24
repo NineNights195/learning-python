fda18a240009c8e33d2cc4a55325e0f82b0d8ed9
NineNights195 <ninenights19534@gmail.com>
2025-10-24 08:32:34 +0700

    Try making a Mad Libs game

---
+++ mini-projects/madlibs.py
@@ -0,0 +1,13 @@
+# 24/10/2025 (Difficulty: Easy)
+# Mad Libs is a word game where you create a story by filling in blanks with random words
+
+adjective01 = input("Enter an adjective (description): ")
+noun01 = input("Enter a noun (person, place, animal): ")
+adjective02 = input("Enter an adjective (description): ")
+verb01 = input("Enter a verb ending with -ing: ")
+adjective03 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective01} zoo.")
+print(f"In an exhibit, I saw a {noun01}.")
+print(f"{noun01} was {adjective02} and {verb01}.")
+print(f"I was {adjective03}!")
