11dd816666ef626d66ccba503596ebe3c4cdce8c
NineNights195 <ninenights19534@gmail.com>
2025-11-17 15:02:10 +0700

    Make mini project: Calculator

---
+++ mini-projects/calculator.py
@@ -0,0 +1,12 @@
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+statement = input("Enter statement: ")
+
+if statement == '+':
+    print(f"Output: {x+y}")
+if statement == '-':
+    print(f"Output: {x-y}")
+if statement == '*':
+    print(f"Output: {x*y}")
+if statement == '/':
+    print(f"Output: {x/y}")
